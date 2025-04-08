86bdd2d2151a9aa9c56b297ddcb8e4b299c70b92
Chinmay Pillai <chinmay.pillai@gmail.com>
2025-04-08 22:19:53 +0530

    Improve Comments

--- Homeworks/A3/client.cpp
+++ Homeworks/A3/client.cpp
@@ -8,9 +8,9 @@
 #include <unistd.h>
 
 #define SERVER_PORT 12345  // Server's listening port
-#define CLIENT_PORT 54321  // Source port for client
+#define CLIENT_PORT 54321  // Client's port
 
-// Function to print TCP flags for debugging
+// Function to print TCP header flags
 void print_tcp_flags(struct tcphdr *tcp) {
     std::cout << "[+] TCP Flags: "
               << " SYN: " << tcp->syn
@@ -21,21 +21,10 @@ void print_tcp_flags(struct tcphdr *tcp) {
               << " SEQ: " << ntohl(tcp->seq) << std::endl;
 }
 
-// Function to calculate TCP checksum (simplified)
-unsigned short calculate_checksum(unsigned short *buf, int nwords) {
-    unsigned long sum = 0;
-    while (nwords > 0) {
-        sum += *buf++;
-        nwords--;
-    }
-    sum = (sum >> 16) + (sum & 0xffff);
-    sum += (sum >> 16);
-    return (unsigned short)(~sum);
-}
-
 // Function to send SYN packet to initiate handshake
 void send_syn(int sock, struct sockaddr_in *server_addr) {
-    // Allocate memory for the packet
+
+    // Allocate memory for packet
     char packet[sizeof(struct iphdr) + sizeof(struct tcphdr)];
     memset(packet, 0, sizeof(packet));
 
@@ -44,34 +33,38 @@ void send_syn(int sock, struct sockaddr_in *server_addr) {
     struct tcphdr *tcp = (struct tcphdr *)(packet + sizeof(struct iphdr));
 
     // Fill IP header
-    ip->ihl = 5;
-    ip->version = 4;
-    ip->tos = 0;
-    ip->tot_len = htons(sizeof(packet));
-    ip->id = htons(12345);
-    ip->frag_off = 0;
-    ip->ttl = 64;
+    ip->ihl = 5;                                    // Header length (5 words * 4 bytes/word = 20 bytes)
+    ip->version = 4;                                // IPv4 
+    ip->tos = 0;                                    // Type of service
+    ip->tot_len = htons(sizeof(packet));            //Length of the packet
+    ip->id = htons(12345);                          // Unique ID for the packet
+    ip->frag_off = 0;                               // Fragment offset
+    ip->ttl = 64;                                   // Time to live
     ip->protocol = IPPROTO_TCP;
-    ip->saddr = inet_addr("127.0.0.1");  // Client address
-    ip->daddr = server_addr->sin_addr.s_addr;
+    // Send ACK from client (source) to server (destination)
+    ip->saddr = inet_addr("127.0.0.1");             // Client address
+    ip->daddr = server_addr->sin_addr.s_addr;       // Server address
 
     // Fill TCP header
-    tcp->source = htons(CLIENT_PORT);
-    tcp->dest = htons(SERVER_PORT);
-    tcp->seq = htonl(200);  // Initial sequence number as specified
-    tcp->ack_seq = 0;
-    tcp->doff = 5;  // Data offset (5 * 4 = 20 bytes)
-    tcp->syn = 1;   // SYN flag
-    tcp->ack = 0;   // Not an ACK
-    tcp->window = htons(8192);
-    tcp->check = 0;  // Kernel will compute checksum if needed
-    tcp->urg_ptr = 0;
+    tcp->source = htons(CLIENT_PORT);               // Source is Client port
+    tcp->dest = htons(SERVER_PORT);                 // Destination is Server port    
+    tcp->seq = htonl(200);                          // Set sequence number to 200 for SYN packet
+    tcp->ack_seq = 0;                               // Not an ACK
+    tcp->doff = 5;                                  // Data offset (5 words * 4 bytes/word = 20 bytes)
+    //Set Appropriate Flags
+    tcp->syn = 1;                                   // SYN flag true
+    tcp->ack = 0;                                   // Not an ACK
+    tcp->window = htons(8192);                      // Window size
+    tcp->check = 0;                                 // Kernel will compute checksum if needed
+    // tcp->urg_ptr = 0;
 
     // Send packet
     if (sendto(sock, packet, sizeof(packet), 0, (struct sockaddr *)server_addr, sizeof(*server_addr)) < 0) {
+        // If sendto fails, print error message and exit
         perror("sendto() failed");
         exit(EXIT_FAILURE);
     } else {
+        // If sendto succeeds, print the sent packet information
         std::cout << "[+] Sent SYN packet with SEQ=" << ntohl(tcp->seq) << std::endl;
     }
 }
@@ -87,34 +80,40 @@ void send_ack(int sock, struct sockaddr_in *server_addr, struct tcphdr *received
     struct tcphdr *tcp = (struct tcphdr *)(packet + sizeof(struct iphdr));
 
     // Fill IP header
-    ip->ihl = 5;
-    ip->version = 4;
-    ip->tos = 0;
-    ip->tot_len = htons(sizeof(packet));
-    ip->id = htons(12346);
-    ip->frag_off = 0;
-    ip->ttl = 64;
-    ip->protocol = IPPROTO_TCP;
-    ip->saddr = inet_addr("127.0.0.1");  // Client address
-    ip->daddr = server_addr->sin_addr.s_addr;
+    ip->ihl = 5;                                    // Header length (5 words * 4 bytes/word = 20 bytes)
+    ip->version = 4;                                // IPv4
+    ip->tos = 0;                                    // Type of service
+    ip->tot_len = htons(sizeof(packet));            // Length of the packet
+    ip->id = htons(12346);                          // Unique ID for the packet
+    ip->frag_off = 0;                               // Fragment offset
+    ip->ttl = 64;                                   // Time to live
+    ip->protocol = IPPROTO_TCP;                     // TCP protocol
+    // Send ACK from client (source) to server (destination)
+    ip->saddr = inet_addr("127.0.0.1");             // Client address
+    ip->daddr = server_addr->sin_addr.s_addr;       // Server address
 
     // Fill TCP header
-    tcp->source = htons(CLIENT_PORT);
-    tcp->dest = htons(SERVER_PORT);
-    tcp->seq = htonl(600);  // Sequence number as required
-    tcp->ack_seq = htonl(ntohl(received_tcp->seq) + 1);  // ACK server's sequence number + 1
-    tcp->doff = 5;  // Data offset
-    tcp->syn = 0;   // Not SYN
-    tcp->ack = 1;   // ACK flag
-    tcp->window = htons(8192);
-    tcp->check = 0;  // Kernel will compute checksum if needed
-    tcp->urg_ptr = 0;
+
+    //Set source and destination ports by switching received TCP ports
+    tcp->source = received_tcp->dest;               // Source port
+    tcp->dest = received_tcp->source;               // Destination port
+    tcp->seq = htonl(600);                          // Set sequence number to 600
+    tcp->ack_seq = htonl(ntohl(received_tcp->seq) + 1);  // Set ACK seq number to server's sequence number + 1
+    //Set Appropriate Flags
+    tcp->doff = 5;                                  // Data offset (5 words * 4 bytes/word = 20 bytes)
+    tcp->syn = 0;                                   // Not SYN
+    tcp->ack = 1;                                   // ACK flag
+    tcp->window = htons(8192);                      // Window size
+    tcp->check = 0;                                 // Kernel will compute the checksum
+    // tcp->urg_ptr = 0;
 
     // Send packet
     if (sendto(sock, packet, sizeof(packet), 0, (struct sockaddr *)server_addr, sizeof(*server_addr)) < 0) {
+        // If sendto fails, print error message and exit
         perror("sendto() failed");
         exit(EXIT_FAILURE);
     } else {
+        // If sendto succeeds, print the sent packet information
         std::cout << "[+] Sent ACK packet with SEQ=" << ntohl(tcp->seq) 
                   << " and ACK=" << ntohl(tcp->ack_seq) << std::endl;
     }
@@ -153,12 +152,17 @@ int main() {
 
     // Wait for SYN-ACK and send final ACK
     while (!handshake_complete) {
+
+        // Receive packet
         int data_size = recvfrom(sock, buffer, sizeof(buffer), 0, (struct sockaddr *)&source_addr, &addr_len);
+
+        // Check if packet was successfully received
         if (data_size < 0) {
             perror("Packet reception failed");
             continue;
         }
 
+        // Cast buffer to IP and TCP headers
         struct iphdr *ip = (struct iphdr *)buffer;
         struct tcphdr *tcp = (struct tcphdr *)(buffer + (ip->ihl * 4));
 
@@ -170,13 +174,16 @@ int main() {
         // Print received packet information
         print_tcp_flags(tcp);
 
-        // Check if it's a SYN-ACK packet with expected sequence number (400)
+        // If it's a SYN-ACK packet with expected sequence number (400)
+        // then print and send final ACK to complete the handshake
         if (tcp->syn == 1 && tcp->ack == 1 && ntohl(tcp->seq) == 400) {
             std::cout << "[+] Received SYN-ACK from server with SEQ=" << ntohl(tcp->seq) 
                       << " and ACK=" << ntohl(tcp->ack_seq) << std::endl;
             
             // Send final ACK to complete the handshake
             send_ack(sock, &server_addr, tcp);
+
+            // Update flag to get out of the loop
             handshake_complete = true;
         }
     }
